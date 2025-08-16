be138ea3c1bc2a57525bda23471ad61edeacda67
thevishwajeetkumar <kumarvishwajeet7638@gmail.com>
2025-08-16 05:33:52 +0530

    next-commit

---
+++ app/api/inngest/route.js
@@ -0,0 +1,11 @@
+import { serve } from "inngest/next";
+
+import { inngest } from "@/lib/inngest/client";
+import { generateIndustryInsights } from "@/lib/inngest/functions";
+
+export const { GET, POST, PUT } = serve({
+  client: inngest,
+  functions: [
+    generateIndustryInsights
+],
+});
